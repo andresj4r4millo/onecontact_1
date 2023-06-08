8df92204bb14478f02b55f048f22eba95e7df807
andresj4r4millo <134433622+andresj4r4millo@users.noreply.github.com>
2023-06-08 17:58:26 -0500

    w

--- poliedro1.py
+++ poliedro1.py
@@ -211,10 +211,12 @@ def forms2(correo,plan,reglist,selleccion,numlist,minimo):
             time.sleep(1)
             #NUMERO DE TELEFONO 
             div_info=driver.find_element(By.ID,'group_4')
+            print(div_info.text)
             nuevo=driver.find_element(By.XPATH,'//*[@id="select2-PhoneId-container"]')
             phone=driver.find_element(By.XPATH,'//*[@id="PhoneId"]')
-             #//*[@id="select2-PhoneId-container"]
-            if nuevo in div_info.find_elements(By.XPATH, './/*'):
+            #//*[@id="select2-PhoneId-container"]
+            div_info_id = div_info.get_attribute("id")
+            if div_info_id == "group_4" and nuevo.text == "Nuevo...":
                 nuevo.click()
                 #escribir nuevo: 
                 driver.find_element(By.XPATH,"/html/body/span/span/span[1]/input").send_keys("NUEVO")#/html/body/span/span/span[1]/input
@@ -236,7 +238,7 @@ def forms2(correo,plan,reglist,selleccion,numlist,minimo):
                 time.sleep(1)
                 driver.find_element('xpath','//*[@id="PhoneNumber"]').send_keys(1111111)
                 time.sleep(1)    
-            elif phone in div_info.find_elements(By.XPATH, './/*'):
+            elif div_info_id == "group_4" and phone.text == "Nuevo...":
                 phone.click.click()
                 driver.find_element('xpath','//*[@id="select2-PhoneId-container"]').click()
                 time.sleep(1)
