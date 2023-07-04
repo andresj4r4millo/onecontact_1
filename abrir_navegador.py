a18d6a093aad1f76efc969ec4e9d603c71ac7fe9
andresj4r4millo <134433622+andresj4r4millo@users.noreply.github.com>
2023-07-04 08:06:37 -0500

    no

---
+++ abrir_navegador.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+#para el libro de exporte
+import openpyxl
+from unidecode import unidecode
+import re
+import pandas as pd
+import numpy as np
+from lxml import html
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+
+
+
+driver = webdriver.Edge('msedgedriver.edge')
+listanumeros=[123,1234,12345,12346]
+numero=123
+
+for i in listanumeros:
+    driver.get(f"google.com{}")
